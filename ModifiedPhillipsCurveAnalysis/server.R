c23fd2daeda79f607511cd88a12625f12eed6055
jeffreyreeve <jeffrey.m.reeve@gmail.com>
2023-01-21 07:50:26 -0600

    final version

--- ModifiedPhillipsCurveAnalysis/server.R
+++ ModifiedPhillipsCurveAnalysis/server.R
@@ -7,7 +7,7 @@ shinyServer(function(input, output) {
   output$UIScatterPlot <- renderPlot({
     
     datecutoff <- datelist[[input$select]]
-    ggplot(UnemploymentInflationRDS[which(UnemploymentInflationRDS$date>= datecutoff[1] & UnemploymentInflationRDS$date<=datecutoff[2]),], aes(x=unemployment_rate, y=inflation_rate)) + geom_point(pch=16, cex=2) + geom_smooth(method=lm, se=FALSE) + geom_point(color = "orange") + theme_bw() + ggtitle("Unemployment vs. Inflation") + theme(plot.title = element_text(hjust = 0.5)) + xlab("Unemployment Rate (%)") + ylab("Inflation Rate (PCEPI) %")
+    ggplot(UnemploymentInflationRDS[which(UnemploymentInflationRDS$date>= datecutoff[1] & UnemploymentInflationRDS$date<=datecutoff[2]),], aes(x=unemployment_rate, y=inflation_rate)) + geom_point(pch=16, cex=2) + geom_smooth(method="lm", formula = "y ~ log(x)", se=FALSE) + geom_point(color = "orange") + theme_bw() + ggtitle("Unemployment vs. Inflation") + theme(plot.title = element_text(hjust = 0.5)) + xlab("Unemployment Rate (%)") + ylab("Inflation Rate (PCEPI) %")
     
   })
   output$UIoverTimePlot <- renderPlot({
@@ -26,4 +26,4 @@ shinyServer(function(input, output) {
       geom_line(aes(y = unemployment_rate), color = "blue", linewidth=0.8) + geom_line(aes(y = inflation_rate), color = "orange", linewidth=0.8) + geom_rect(data=recessionRDS, aes(x=NULL, y=NULL, xmin=lubridate::as_date(Peak), xmax=lubridate::as_date(Trough), ymin=-Inf, ymax=+Inf), alpha=0.2) + theme_bw() + ggtitle("Phillips Theory True During Recessions: Unemployment (blue), Inflation (orange), Recessions (grey)") + theme(plot.title = element_text(hjust = 0.5)) + xlab("Date") + ylab("Unemployment, Inflation (PCEPI), %")
   })  
   
-})
+}) 
